5110db46a19c492e5d74cd97dc1a50bf02aae5d2
Aleks Lukin <al.lykin@mail.ru>
2024-12-17 01:40:27 +0300

    alfa test header+hero

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
-import '../style/style.scss';
+document.addEventListener('DOMContentLoaded', () => {
+    const headerNavLinks = document.querySelectorAll('.link_style');
+    
+
+    const handleClickHeaderNavLinks = (event) => {
+        event.preventDefault();
+        event.target.classList.add('link_style_clicked');
+    };
+
+    const handleMouseIn = (event) => {
+        event.target.classList.add('link_style_hovered');
+    };
+
+    const handleMouseOut = (event) => {
+        event.target.classList.remove('link_style_hovered');
+    };
+
+
+    headerNavLinks.forEach((headerLink) => {
+        headerLink.addEventListener('mouseover', handleMouseIn);
+        headerLink.addEventListener('mouseout', handleMouseOut);
+        headerLink.addEventListener('click', handleClickHeaderNavLinks);
+
+    });
+
+    const headerIcons = document.querySelectorAll('.header_login-cart_styles');
+
+    const handleClickHeaderIcons = (event) => {
+        event.preventDefault();
+        event.target.classList.add('header_login-cart_styles_clicked');
+    };
+
+    headerIcons.forEach((headerIcon) => {
+        headerIcon.addEventListener('click', handleClickHeaderIcons);
+    });
+
+});
+
+
