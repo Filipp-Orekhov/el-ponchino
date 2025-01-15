54f0574c4f15df54560de3f537746b6d30e4eed2
Filipp-Orekhov <filipp.orekhov85@gmail.com>
2025-01-15 22:36:17 +0300

    resolved

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,7 +2,7 @@ const iconMenu = document.querySelector('.menu_icon');
 const menuBody= document.querySelector('.header_nav');
 
 iconMenu.addEventListener('click', () => {
-    iconMenu.classList.toggle('active');
-    menuBody.classList.toggle('active');
-    document.body.classList('_lock');
-})
+            iconMenu.classList.toggle('active');
+            menuBody.classList.toggle('active');
+            document.body.classList('_lock');
+    })
